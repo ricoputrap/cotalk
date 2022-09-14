3c8a51f5e488fc610e1fa8ea8cedadd5374dd04c
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-09-14 21:24:46 +0700

    +T3 fix useTotalNewMessages custom hook to calculate only not read messages

--- hooks/useTotalNewMessages.ts
+++ hooks/useTotalNewMessages.ts
@@ -11,7 +11,7 @@ const useTotalNewMessages = (roomID: string): number => {
     if (roomID == activeRoomID)
       return 0;
 
-    const messageReceived: Message[] = messagePerRoom[roomID]?.filter(msg => !msg.fromSender);
+    const messageReceived: Message[] = messagePerRoom[roomID]?.filter(msg => !msg.fromSender && !msg.isRead);
     return messageReceived?.length || 0;
   }, [roomID, activeRoomID, messagePerRoom]);
 
